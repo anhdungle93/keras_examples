a17330ddf4a63acca15f3f8db607a8ab5b5bdf6a
anhdungle <anhdungle634@gmail.com>
2020-06-17 17:19:33 +0200

    add a CNN for image recognition

--- Cat_and_Dog_classifier.py
+++ Cat_and_Dog_classifier.py
@@ -45,6 +45,7 @@ for features, label in training_data:
     y.append(label)
 
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+y = np.array(y)
 
 # %%
 import pickle
@@ -53,11 +54,47 @@ pickle_out = open("data/kaggle_cats_and_dogs/processed_data/X.pickle", "wb")
 pickle.dump(X, pickle_out)
 pickle_out.close()
 
-pickle_out = open("data/kaggle_cats_and_dogs/processed_data/y.pikcle", "wb")
+pickle_out = open("data/kaggle_cats_and_dogs/processed_data/y.pickle", "wb")
 pickle.dump(y, pickle_out)
 pickle_out.close()
 
 
 # %%
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+
+X = pickle.load(open("data/kaggle_cats_and_dogs/processed_data/X.pickle", "rb"))
+y = pickle.load(open("data/kaggle_cats_and_dogs/processed_data/y.pickle", "rb"))
+
+X = X / 255
+
+model = Sequential()
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss="binary_crossentropy",
+                optimizer="adam",
+                metrics=['accuracy'])
+
+model.fit(X, y, batch_size=32, validation_split=0.1, epochs=10)
 
 
+
+
+
+# %%
+model.save("cats_and_dogs_classifier.model")
+
+# %%
