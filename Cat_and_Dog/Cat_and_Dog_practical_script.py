fix bug of not normalizing in practical_script

Cat_and_Dog/Cat_and_Dog_practical_script.py
@@ -1,6 +1,10 @@
 #%%
 import cv2
 import tensorflow as tf
+import sys
+sys.path.append('..')
+import helping_script as hs
+import numpy as np
 
 #%%
 CATEGORIES = ["Dog", "Cat"]
@@ -8,63 +12,19 @@ CATEGORIES = ["Dog", "Cat"]
 def prepare(filepath):
     IMG_SIZE = 75
     img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE)) / 255
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 #%%
 model = tf.keras.models.load_model("cats_and_dogs_classifier.model")
 #%%
-print(model.summary())
-#%%
-prediction = model.predict([prepare('dog.jpg')])
+_input = prepare('landscape.jpg')
 
 # %%
-prediction
-
-# %%
-prediction = model.predict([prepare('cat.jpg')])
-
-
-# %%
-prediction
-
-# %%
-prediction = model.predict_proba([prepare('landscape.jpg')])
-
-
-# %%
-prediction[0]
-
-# %%
-prediction[0][0]
-
-# %%
-for layer in model.layers:
-    print(layer.name)
-# %%
-
-#%%
-
-# %%
-from tensorflow.keras import Model
-intermediate_layer_model = Model(inputs=model.input, outputs=model.get_layer("dense_5").output)
-
-# %%
-model.input
-
-# %%
-intermediate_output = intermediate_layer_model([prepare('cat.jpg')])
-
-# %%
-import numpy as np
-np.array(intermediate_output)
-
-# %%
-model.layers
+outputs = hs.get_output_of_all_layers(model,_input)
 
 # %%
 print(model.summary())
-
 # %%
-model.layers[4]
+outputs["activation_8"]
 
 # %%

helping_script.py
@@ -1,4 +1,5 @@
 from tensorflow.keras import Model
+import numpy as np
 
 def get_intermediate_layer(model, layer_name, test_input):
     return Model(inputs=model.input, outputs=model.get_layer(layer_name).output)
@@ -9,12 +10,12 @@ def get_intermediate_output(model, layer_name, test_input):
 
 
 def get_output_of_all_layers(model, test_input):
-    output_of_all_layers = []
+    output_of_all_layers = dict()
     for count, layer in enumerate(model.layers):
-
+        layer_name = layer.name
         #skip the input layer
         if count == 0:
             continue
-        output_of_all_layers.append(get_intermediate_output(model, layer, test_input))
+        output_of_all_layers[layer_name] = np.array(get_intermediate_output(model, layer_name, test_input))
     
     return output_of_all_layers